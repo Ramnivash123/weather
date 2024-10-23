4206546397ff30213877c56e97f92e3fc2f8323b
Ramnivash SK <115456745+Ramnivash123@users.noreply.github.com>
2024-10-23 18:26:17 +0530

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,7 +1,15 @@
-﻿namespace YourAppNamespace.Models
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace YourAppNamespace.Models
 {
     public class User
     {
+        // This will map the _id field in MongoDB to the Id property in C#
+        [BsonId]  // Indicates that this property is the MongoDB _id field
+        [BsonRepresentation(BsonType.ObjectId)]  // Converts ObjectId to string for ease of use
+        public string Id { get; set; } = string.Empty;
+
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
